04076f4cac107de2d0a92e9c59434bdaf20c085d
Robbie Paul <robbie.paul@dxw.com>
2020-09-04 16:16:30 +0100

    Add prettier lint commands and run lint

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-var express = require('express');
+var express = require("express");
 var app = express();
 
-app.get('/', function(req, res){
-   res.send('Hello dxw!');
+app.get("/", function (req, res) {
+  res.send("Hello dxw!");
 });
 
 module.exports = { app };
